b8c2d95940455d6492ae9a8a235f24f6b6436d48
adam-p-king <devadamking@gmail.com>
2017-04-11 09:40:28 +0100

    Add missing lang/js files

---
+++ lang/en/local_lessonexportepub.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings
+ *
+ * @package   local_lessonexportepub
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['created'] = 'Created on {$a}';
+$string['exportepub'] = 'Export as epub';
+$string['failedinsertimage'] = 'Failed to insert image: {$a}';
+$string['filename'] = 'Export {$a->lessonname} {$a->timestamp}';
+$string['modified'] = 'Last modified by {$a->modifiedby} on {$a->timemodified}';
+$string['plugingroup'] = 'Lesson Export';
+$string['pluginname'] = 'Lesson Export EPUB';
+$string['printed'] = 'This document was downloaded on {$a}';
+$string['publishername'] = 'Unknown';
+$string['lessonexportepub:export'] = 'Export lesson as epub';
+$string['lessonexportfailed'] = 'Export of lesson \'{$a}\' failed';
+$string['lessonexportfailed_body'] = 'The lesson \'{$a->name}\' has been updated, but the attempt to export and email it has failed, after {$a->exportattempts} attempts. If the lesson is updated again, then further attempts will be made to export it. The Lesson can be found at: {$a->url}.';
+$string['lessonupdated'] = 'Lesson \'{$a}\' updated';
+$string['lessonupdated_body'] = 'Updated export attached';
+
+$string['customstyle'] = 'Custom EPUB style rules';
+$string['customstyle_desc'] = 'Custom CSS to apply only to the exported EPUB';
+$string['exportstrict'] = 'Strict exporting';
+$string['exportstrict_desc'] = 'Whether documents should throw exceptions or suppress them when erroring.';
+$string['covercolour'] = 'Front cover banner colour';
+$string['covercolour_desc'] = 'The colour of the banner on the front cover that contains the lesson title.';
