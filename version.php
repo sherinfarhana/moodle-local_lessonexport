0b2a65b000824a99456f86aabee39e5cd2846654
adam-p-king <devadamking@gmail.com>
2017-04-11 11:10:04 +0100

    Version bump for release

--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017033000;
+$plugin->version   = 2017041100;
 $plugin->requires  = 2014051200; // Moodle 2.7.
 $plugin->cron      = DAYSECS;
 $plugin->component = 'local_lessonexport';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "2.7+ (Build: 2017033000)";
+$plugin->release   = "2.7+ (Build: 2017041100)";
 
 $plugin->dependencies = array(
     'mod_lesson' => ANY_VERSION,
